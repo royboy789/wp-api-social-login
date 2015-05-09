087476b11d13bee2a88d8cc8e4eab677e07da087
Roy Sivan <roy.x.sivan.-ND@disney.com>
2015-05-09 13:11:44 -0700

    Starting social connections
    Facebook and GitHub working
    Twitter returning 401 - need to debug

--- assets/js/social.js
+++ assets/js/social.js
@@ -1,7 +1,72 @@
 var $ = jQuery;
 
+
+/** FACEBOOK **/
+function facebook_connect(){
+	hello("facebook").login().then( function() {
+		console.log('logged in to Facebook');
+		hello("facebook").api("me").then(function(json){
+			console.log( json );
+			console.log( 'Facebook json.id: ' + json.id );
+		});
+	},function(e){
+		console.log(e);
+	});	
+}
+
+
+/** TWITTER **/
+function twitter_connect(){
+	hello("twitter").login().then( function() {
+		console.log('logged in to Twitter');
+		hello("twitter").api("me").then(function(json){
+			console.log( json );
+		});
+	},function(e){
+		console.log(e);
+	});	
+}
+
+
+/** GITHUB **/
+function github_connect(){
+	hello("github").login().then( function() {
+		console.log('logged in to Twitter');
+		hello("github").api("me").then(function(json){
+			console.log( json );
+			console.log( 'GitHub json.id: ' + json.id );
+		});
+	},function(e){
+		console.log(e);
+	});	
+}
+
+
 $(document).ready(function(){
 	
-	console.log('here');
+	hello.init({
+		facebook: "907221609321148",
+		twitter: "5GRbsejpkoenSXnFRxu93CqVH",
+		github: "490074a2ebeecd8ff906",
+		google: "870684892978-c1mguuas1d6s0lpde78t17v6fusivfl9@developer.gserviceaccount.com"
+	}, {
+		scope: 'email'
+	});
+	
+	$('body').on('click', '.facebook-login', function(e){
+		e.preventDefault();
+		facebook_connect();		
+	});
+	
+	$('body').on('click', '.twitter-login', function(e){
+		e.preventDefault();
+		twitter_connect();		
+	});
+	
+	$('body').on('click', '.github-login', function(e){
+		e.preventDefault();
+		github_connect();		
+	});
+			
 	
 })
